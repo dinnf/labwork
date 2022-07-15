4c5e1a28f3c37e9dc4b37c7475d2baf486f7c195
Thor P <71690397+thorxx0011@users.noreply.github.com>
2022-07-16 10:06:39 +1200

    Adding Arrays

--- Thor Pedersen/Classwork/JavaScript/Variables.js
+++ Thor Pedersen/Classwork/JavaScript/Variables.js
@@ -10,3 +10,25 @@ let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
 console.log(mountains[0]); // 'Everest'
 console.log(mountains[1]); // 'Fuji'
 console.log(mountains[2]); // 'Nanga Parbat'
+
+let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+console.log(seas);
+seas.push('Red Sea');
+console.log(seas);
+seas.unshift('Thors Sea');
+console.log(seas);
+seas.pop();
+console.log(seas);
+let NorthSeaIndex = seas.indexOf('North Sea');
+console.log(NorthSeaIndex); // 2
+
+// Exercise Array
+
+//For understanding more about arrays try creating an array that
+//has 5 elements.
+let heroes = ["Thor", "Superman", "Spiderman", "Batman", "Ironman"];
+//• Replace the value of the element at position 1 and 4.
+console.log(heroes);
+heroes[1] = "Hulk";
+heroes[4] = "Thomas the Tank Engine";
+console.log(heroes);
