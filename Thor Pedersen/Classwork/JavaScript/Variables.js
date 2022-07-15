41576665bc9b8aa7ed1332245455d90a968a3efa
Thor P <71690397+thorxx0011@users.noreply.github.com>
2022-07-16 09:52:41 +1200

    adding Array

--- Thor Pedersen/Classwork/JavaScript/Variables.js
+++ Thor Pedersen/Classwork/JavaScript/Variables.js
@@ -5,3 +5,8 @@ let hello;
 check > 3 ? hello ="Say Hello instead" : "say Hi";
 
 console.log(hello);
+
+let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
+console.log(mountains[0]); // 'Everest'
+console.log(mountains[1]); // 'Fuji'
+console.log(mountains[2]); // 'Nanga Parbat'
