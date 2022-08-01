eeeb5c60e9732fa1010c2be1c3727d36fa4ca5ea
Mann Badal <mann.badal@icloud.com>
2022-08-02 11:29:12 +1200

    Lab four completed

--- Mann Badal/LabFour/assets/script.js
+++
@@ -1,6 +0,0 @@
-let box = document.querySelector('.container');
-let width = box.offsetWidth;
-let height = box.offsetHeight;
-console.log({ width, height });
-
-document.getElementById("footer").innerHTML = `style="width:${width};"`
