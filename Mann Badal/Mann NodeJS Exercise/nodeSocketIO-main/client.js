7aef059a9d857f7549ce1d6deed0ae06aa7f0640
Mann Badal <mann.badal@icloud.com>
2022-09-24 12:13:04 +1200

    added nodeSocketIO exercise

---
+++ Mann Badal/Mann NodeJS Exercise/nodeSocketIO-main/client.js
@@ -0,0 +1,19 @@
+let io = require('socket.io-client');
+
+const socket = io('ws://127.0.0.1:3000');
+
+console.log("connection to server ");
+
+
+socket.on("Topic1", (arg) => {
+    console.log("Message from Server - hello : "+arg);
+})
+
+
+socket.emit("howdy", " I am a Client");
+
+
+
+socket.on("number", (arg) => {
+    console.log("Message from Server - number : "+arg);
+})
---
+++ Mann Badal/Mann NodeJS Exercise/nodeSocketIO-main/server.js
@@ -0,0 +1,40 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+//let port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.send("<h1>App is running </h1>");
+})
+
+
+console.log("opening a stocket ");
+
+io.on("connection", (socket) => {
+    //send a message to client
+    socket.emit("Topic1", "Weather Information");
+
+    //receive a message from client
+    socket.on("howdy", (arg) => {
+        console.log('Message received from client  - howdy : '+ arg);
+    });
+
+    setInterval(()=>{
+        let anum = parseInt(Math.random()*10);
+        console.log("A number sent to client : "+anum)
+        socket.emit('number', anum);
+      }, 4000);
+    
+      setInterval(()=>{
+        let anum = parseInt(Math.random()*10);
+        console.log("A 2ndnumber sent to client : "+anum)
+        socket.emit('2ndnumber', anum);
+      }, 6000);
+
+});
+server.listen(3000);
